b117ab573f0891c69c0bb720266b58c778ee96e7
DenisAlexanderov <133494421+DenisAlexanderov@users.noreply.github.com>
2023-06-05 21:33:14 +0300

    Add files via upload
    Добавил 1-ю задачу второго уровня

---
+++ lvl_2task_2.1.py.py
@@ -0,0 +1,12 @@
+A = []
+for _ in range(5)
+    a = int(input('Введите целое число...'))
+    A.append(int(a))
+x = A[0]
+y = A[0]
+for i in A
+    if x  i
+        x = i
+    if y  i
+        y = i
+print(f'Максимальное {x}' , f'Минимальное {y}' )
