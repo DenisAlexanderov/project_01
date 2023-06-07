7288360e91db14d1128c7d1b63e7cc136530994e
DenisAlexanderov <133494421+DenisAlexanderov@users.noreply.github.com>
2023-06-07 11:54:10 +0300

    Add files via upload
    Добавлена задача 2.3

---
+++ task 2_3.py
@@ -0,0 +1,25 @@
+ # Задача 2.3.
+
+# Напишите функцию, которая принимает цифры от 0 до 9 и возвращает значение прописью.
+# Например,
+# switch_it_up(1) -> 'One'
+# switch_it_up(3) -> 'Three'
+# switch_it_up(10000) -> None
+# Использовать условный оператор if-elif-else нельзя! 
+cell=input("Введите число от 0 до 9:")
+cell_number=int(cell)   
+ switch_it_up = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine'
+
+}
+
+print('Введенное число:',switch_it_up.get(cell_number))
